29a6a687d8dc83cee83a295eedad1c765a4c29b7
tmsachith <sachith.icc@gmail.com>
2024-12-31 13:29:31 +0530

    backend edit

--- node-backend/server.js
+++ node-backend/server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
